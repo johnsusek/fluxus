0b35bdbc510782e42423660e86a00f299ab7d79e
John Susek <john@johnsolo.net>
2019-06-15 18:04:10 -0500

    Make rootstate a bindable object so subclasses don't have to

--- Sources/Fluxus/RootState.swift
+++ Sources/Fluxus/RootState.swift
@@ -1,3 +1,9 @@
-open class RootState {
+import SwiftUI
+import Combine
+
+@available(OSX 10.15, iOS 13, *)
+open class RootState: BindableObject {
+  public var didChange = PassthroughSubject<Void, Never>()
+
   public init() {}
 }
